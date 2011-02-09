7f6815f125cdd58ba9fd6fe0f69a47ae8e0f1a71
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2011-02-09 17:36:29 -0200

    Fixing Portal o dia fixes

--- lib/readability.rb
+++ lib/readability.rb
@@ -94,13 +94,17 @@ module Readability
 
     def odia
       debug "I have an O Dia page"
-      extracted = @document.css("#content-noticia p, #content-noticia img")
+      extracted = @document.css("#content-noticia p")
       extracted.each do |elem|
         if (elem.try(:inner_text) =~ /^\W*$/)
           extracted.delete elem
         end
-        if elem.try(:name) == "img"
-          elem.set_attribute("src", "http://"+ @base_uri + elem.attribute("src").value)
+        if elem.try(:children)
+          elem.children.each do |child|
+            if child.try(:name) == "img"
+              child.set_attribute("src", "http://"+ @base_uri + child.attribute("src").value)
+            end
+          end
         end
       end
       extracted
