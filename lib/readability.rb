36932bb9666a90f3cef9a6d72e728624c70fb8cb
Matthias Wagner <matthias.wagner.office@googlemail.com>
2011-12-23 03:54:39 -0800

    fix semicolon

--- lib/readability.rb
+++ lib/readability.rb
@@ -77,7 +77,7 @@ module Readability
 
         cleaned_article = sanitize(article, candidates, options)
 
-        cleaned_article.gsub!(/[^\x00-\x7FÁáÉéÍíÑñÓóÚúÜü]/,'') if cleaned_article
+        cleaned_article.gsub!(/[^\x00-\x7FÁáÉéÍíÑñÓóÚúÜü]/,"'") if cleaned_article
 
         if remove_unlikely_candidates && article.text.strip.length < (options[:retry_length] || RETRY_LENGTH)
           make_html
